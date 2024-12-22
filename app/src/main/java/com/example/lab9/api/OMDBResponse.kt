227d1960768888b780f21928aa6abb7ebe277613
ivansizov <sizovivan.05@gmail.com>
2024-12-22 22:04:04 +0700

    add data class with request and response

---
+++ app/src/main/java/com/example/lab9/api/OMDBResponse.kt
@@ -0,0 +1,20 @@
+package com.example.lab9.api
+
+import com.google.gson.annotations.SerializedName
+
+
+data class OMDBResponse(
+    @SerializedName("Search") val search: List<OMDbMovie>?,
+    @SerializedName("totalResults") val totalResults: String?,
+    @SerializedName("Response") val response: String,
+    @SerializedName("Error") val error: String?
+)
+
+data class OMDbMovie(
+
+    @SerializedName("Title") val title: String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("imdbID") val imdbID: String,
+    @SerializedName("Type") val type: String,
+    @SerializedName("Poster") val poster: String
+)
